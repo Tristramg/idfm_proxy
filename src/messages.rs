f7b856e97bf72fe95675c1a18edce3b784614705
Tristram Gräbener <tristramg@gmail.com>
2022-11-21 19:13:50 +0100

    Utilisation du référentiel des lignes idfm

--- src/messages.rs
+++ src/messages.rs
@@ -1,11 +1,18 @@
 use std::sync::Arc;
 
 use actix::prelude::*;
+use siri_lite::service_delivery::EstimatedVehicleJourney;
 
 #[derive(Message)]
 #[rtype(result = "()")]
 pub struct Connect {
     pub addr: Recipient<DataUpdate>,
+}
+
+#[derive(Message, Clone)]
+#[rtype(result = "()")]
+pub struct SiriUpdate {
+    pub vjs: Vec<EstimatedVehicleJourney>,
 }
 
 #[derive(Message, Clone)]
--- src/objects.rs
+++ src/objects.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use serde::Deserialize;
 use siri_lite::service_delivery::EstimatedVehicleJourney;
 
 pub struct PTData {
@@ -7,9 +8,28 @@ pub struct PTData {
 }
 
 pub struct Line {
-    pub name: String,
-    pub id: String,
-    pub mode: String, // the mode could be different for the lines's VJ, but we'll consider this to be the commercial mode for the moment
-
+    pub reference: LineReference,
     pub vjs: Vec<EstimatedVehicleJourney>,
+}
+
+// Struct to deserialize the data from https://data.iledefrance-mobilites.fr/explore/dataset/referentiel-des-lignes
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct LineReference {
+    #[serde(rename = "ID_Line")]
+    pub id: String,
+    #[serde(rename = "Name_Line")]
+    pub name: String,
+    #[serde(rename = "ShortName_Line")]
+    pub short_name: String,
+    #[serde(rename = "TransportMode")]
+    pub mode: String,
+    #[serde(rename = "TransportSubmode")]
+    pub sub_mode: String,
+    pub operator_name: String,
+    pub network_name: String,
+    #[serde(rename = "ColourWeb_hexa")]
+    pub background_color: String,
+    #[serde(rename = "TextColourWeb_hexa")]
+    pub text_color: String,
 }
